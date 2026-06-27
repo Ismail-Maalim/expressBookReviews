f6d9e203d3df3b80110f0ae899602c45f8104424
ismail maalim <mihnaz682@gmail.com>
2026-06-27 12:23:55 -0400

    getting the book details based on ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,7 +20,14 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn; // Retrieve the ISBN from request parameters
+  
+  if (books[isbn]) {
+    return res.status(200).send(JSON.stringify(books[isbn], null, 4));
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
+  //return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
